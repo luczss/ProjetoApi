198ef5227fd3177d60ceaab462c893ecadaadfe4
lucas <luczssz3@gmail.com>
2026-05-15 09:55:39 -0300

    fix:arrumando o js

--- src/js/script.js
+++ src/js/script.js
@@ -1,60 +1,63 @@
-//Declaraçoes dos elementos com DOM
+//DECLARAÇÕES DOS ELEMENTOS COM DOM
 const videoElemento=document.getElementById("video");
 const botaoScanear=document.getElementById("btn-texto");
-const resultado=document.getElementById("saida");
-const canvas=document.getElementById("canvas");
+const resultado = document.getElementById("saida");
+const canvas = document.getElementById("canvas");
 
-//funcao para habilitar a camera
+//função para habilitar a câmera
 
 async function configurarCamera(){
     try{
-        const midia =await navigator.mediaDevices.getUserMedia({
-            video:{facingMode:"enviroment"},// habilitacao da camera
-            audio:False//o audio nao sera capturado
+        const midia = await navigator.mediaDevices.getUserMedia({
+            video: { facingMode: "environment"}, //habilitando a camera traseira
+            audio:false // o audio não será capturado
         })
         //atribuir o fluxo da camera em midia
-        videoElemento.srcObject= midia;
-        //garante que a camera vai funcionar
-        videoElemento.onplay();
+        videoElemento.srcObject = midia;
+        //Garante que a câmera vai funcionar
+        videoElemento.play();
     }catch(erro){
-            resultado.innerText="Erro ao capturar a câmera"
+            resultado.innerText="Erro ao capturar a câmera",erro
     }
 }
-//executa funcao da camera
+//executa função da câmera
 configurarCamera();
-//funcao para capturar o texto
-botaoScanear.onclick=async()=>{
-    botaoScanear.disable=true;//habilitar o botao para pegar o texto
-    resultado.innerText= "Fazendo a leitura ...aguarde";
-    
+
+//função para capturar o texto
+
+botaoScanear.onclick = async ()=>{
+    botaoScanear.disabled =true;// habilita o botão para pegar o texto
+    resultado.innerText ="Fazendo a Leitura ...aguarde";
+
     //prepara o canvas para receber a estrutura em 2d
     const contexto = canvas.getContext("2d");
 
-    //ajudas o tamanho do canvas de acroo com o video
-    canvas.Width= videoElemento.videoWidth;
-    canvas.Height= videoElemento.videoHeight;
+    //ajusta o tamnho do canvas de acordo com video
+    canvas.width = videoElemento.videoWidth;
+    canvas.height = videoElemento.videoHeight;
 
-    //defini a matriz de trasnsformacao de canvas(escala, inclinacao)
-    contexto.setTransform(1,0,0,1,0,0);
+    //define a matriz de transformação do canvas (escla, inclinação)
+    contexto.setTransform(1, 0, 0, 1, 0, 0);
 
-    //aplica filtro de contrast para melhorar o OCR
-    contexto.fiter='contrast(1.2) grayscale(1)';
-    // desenha o video do canvas
-    contexto.drawImage(videoElemento,0,0, canvas.width,canvas.height);
+    //aplica fitro de contrast para melhorar o OCR
+    contexto.filter = 'contrast(1.2) grayscale(1)';
+
+    //Desenha p video no canvas
+    contexto.drawImage(videoElemento, 0, 0, canvas.width,canvas.height);
 
     try{
-        const{data: {text}}=await tesseract.recognize(
+        //o tesseract retorna o objeto
+        const {data: { text }}= await Tesseract.recognize(
             canvas,
-            'por'
+            'por' //define o idioma
         );
-        //remove todos os espacos em branco
-        const textoFinal=text.trim();
-        resultado.innerText=textoFinal.length >0 ? textoFinal:"Nâo foi possivel indentificar a câmera"
-    }catch{
+        //remove todos os espaços em branco
+        const textoFinal= text.trim();
+        resultado.innerText=textoFinal.length > 0 ? textoFinal :"Não foi possivel identificar o texto";
+    }catch(erro){
         resultado.innerText="Erro ao processar a leitura",erro
     }finally{
-        //desabilitar a leitura do texto para comecar novamente
-        botaoScanear.disable=false;
-
+        //desabilita a leitura do texto para começar novamente.
+        botaoScanear.disabled=false;
     }
 }
